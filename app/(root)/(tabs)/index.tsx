eece4a9d63976a9221de066bba641d25ba16e5ae
Abolfazl <abolfazljamshididev@gmail.com>
2025-12-27 10:32:28 +0330

    configure image type declaration to resolve typescript error

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,8 +10,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg">Welcome to ReState</Text>
-      <Link href={"/sign-in"} className="text-red-500">
+      <Text className="font-bold font-rubik my-10 text-3xl">
+        Welcome to ReState
+      </Text>
+      <Link href={"/sign-in"} className="text-primary-300">
         Sign In
       </Link>
       <Link href={"/explore"}>Explore</Link>
